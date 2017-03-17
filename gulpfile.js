5ed8cd3c49e2aee728eefa81438946bce1a8bf1d
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-03-17 14:20:05 -0300

    Chamando todos os estilos com bower

--- gulpfile.js
+++ gulpfile.js
@@ -17,18 +17,36 @@ gulp.task('build-img',function(){
 });
 
 gulp.task('bootstrap',function(){ //copiando da pasta de origim e colando no destino
+
 	gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css')
 		.pipe(gulp.dest('public/stylesheets'));
+
 	gulp.src('bower_components/bootstrap/dist/js/bootstrap.min.js')
 		.pipe(gulp.dest('public/javascripts'));
+
 	gulp.src('bower_components/bootstrap/dist/fonts/**/*')
 		.pipe(gulp.dest('public/fonts'));
-	gulp.src('bower_components/jquery/dist/jquery.min.js')
-		.pipe(gulp.dest('public/javascripts'));
 	console.log("bootstrap");
 });
 
-gulp.task('default', ['bootstrap','build-img']);
+gulp.task('jquery',function(){ //copiando da pasta de origim e colando no destino
+
+	gulp.src('bower_components/jquery/dist/jquery.min.js')
+		.pipe(gulp.dest('public/javascripts'));
+	console.log("jquery")
+});
+
+gulp.task('fontawesome',function(){ //copiando da pasta de origim e colando no destino
+
+	gulp.src('bower_components/font-awesome/css/font-awesome.min.css')
+		.pipe(gulp.dest('public/stylesheets'));
+		
+	gulp.src('bower_components/font-awesome/fonts/**/*')
+		.pipe(gulp.dest('public/fonts'));
+	console.log("font-awesome");
+});
+
+gulp.task('default', ['bootstrap','jquery','fontawesome','build-img']);
 	
 
 path.join(__dirname)//para saber onde eu estou
