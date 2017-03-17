325b280435ba21dbed09a756af0435af01a4739b
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-03-17 13:54:10 -0300

    Organizando js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,28 +1,35 @@
-$(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
+var trigger = $('.hamburger'),
+	overlay = $('.overlay'),
+	isClosed = false;
 
-    trigger.click(function () {
-      hamburger_cross();      
-    });
+function hamburger_cross() { //trocando classe de menu
+	
+	if (isClosed == true) {          
+		overlay.hide();
+		trigger.removeClass('is-open');
+		trigger.addClass('is-closed');
+		isClosed = false;
+	} else {   
+		overlay.show();
+		trigger.removeClass('is-closed');
+		trigger.addClass('is-open');
+		isClosed = true;
+	}
+}
 
-    function hamburger_cross() {
 
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
+function actions () {
+
+	trigger.click(function () {
+		hamburger_cross();
+	});
   
-  $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-  });  
+	$('[data-toggle="offcanvas"]').click(function () {
+		$('#wrapper').toggleClass('toggled');
+	});  
+}
+
+
+$(document).ready(function () {
+	actions();
 });
