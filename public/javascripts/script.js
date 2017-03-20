921797e41b5e2ff97e9558e4f345467ce6bb9c46
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-03-20 15:05:17 -0300

    inclundo novo menu

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,35 +1,22 @@
-var trigger = $('.hamburger'),
-	overlay = $('.overlay'),
-	isClosed = false;
-
-function hamburger_cross() { //trocando classe de menu
-	
-	if (isClosed == true) {          
-		overlay.hide();
-		trigger.removeClass('is-open');
-		trigger.addClass('is-closed');
-		isClosed = false;
-	} else {   
-		overlay.show();
-		trigger.removeClass('is-closed');
-		trigger.addClass('is-open');
-		isClosed = true;
-	}
-}
 
 
-function actions () {
-
-	trigger.click(function () {
-		hamburger_cross();
+function menu(){
+	$('#nav-icon1').click(function(){
+		$(this).toggleClass('open');
 	});
-  
+}
+function abrir_fechar(){
 	$('[data-toggle="offcanvas"]').click(function () {
 		$('#wrapper').toggleClass('toggled');
 	});  
 }
 
+function actions () {
+	menu();
+	abrir_fechar(); 
+}
 
 $(document).ready(function () {
 	actions();
+
 });
