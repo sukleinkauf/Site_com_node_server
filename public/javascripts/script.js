632f3f78f6c9701d7ed5b051f2a4a26e312d1907
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-03-21 10:17:06 -0300

    menu pronto

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,19 +1,37 @@
+var trigger = $('.hamburger'),
+	overlay = $('.overlay'),
+	isClosed = false;
 
+function hamburger_cross() {
 
-function menu(){
-	$('#nav-icon1').click(function(){  //função que muda botão
-		$(this).toggleClass('open');
-	});
+	if (isClosed == true) {          
+		overlay.hide();
+		trigger.removeClass('is-open');
+		trigger.addClass('is-closed');
+		isClosed = false;
+	} else{
+		overlay.show();
+		trigger.removeClass('is-closed');
+		trigger.addClass('is-open');
+		isClosed = true;
+	}
 }
+
 function abrir_fechar(){
 	$('[data-toggle="offcanvas"]').click(function () { //função que abre e fecha menu
 		$('#wrapper').toggleClass('toggled');
 	});  
 }
+// function tituloHistória(){
+	
+// }
 
 function actions () {
-	menu();
-	abrir_fechar(); 
+	abrir_fechar();
+
+	trigger.click(function () {
+		hamburger_cross();      
+	}); 
 }
 
 $(document).ready(function () {
