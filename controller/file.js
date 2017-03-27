5d29040cc8e8565deeda3f3a24988e582de44257
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-03-27 10:38:06 -0300

    controller

---
+++ controller/file.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+
+module.exports ={
+	read: function(callback){
+		fs.readFile(__dirname + "/../" + "db/products.json", 'utf8',function(err, data){
+			if(err)
+				return console.log(err);
+			data = JSON.parse(data);
+			callback(data);
+		});
+	},
+
+	// write: function(dataJson, res){
+	// 	fs.writeFile(__dirname + "/../" + 'db/products.json', dataJson, function(err){
+	// 		if(err)
+	// 			return console.log(err);
+	// 		res.json({'msg': 'Usuário inserido com sucesso!'});
+	// 	});
+	// }
+}
