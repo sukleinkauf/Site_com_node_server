891b65a7460dddf25e10dde6c93979a77558436a
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-03-30 16:21:25 -0300

    read de encomendas

--- controller/file.js
+++ controller/file.js
@@ -9,6 +9,14 @@ module.exports ={
 			callback(data);
 		});
 	},
+	readEncomendas: function(callback){
+		fs.readFile(__dirname + "/../" + "db/encomendas.json", 'utf8',function(err, data){
+			if(err)
+				return console.log(err);
+			data = JSON.parse(data);
+			callback(data);
+		});
+	},
 
 	// write: function(dataJson, res){
 	// 	fs.writeFile(__dirname + "/../" + 'db/products.json', dataJson, function(err){
