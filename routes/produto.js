efa18696dd22f34c5eb42aea66ef0a82fc5aa88f
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-04-03 16:48:38 -0300

    produto.js

--- routes/produto.js
+++ routes/produto.js
@@ -8,7 +8,13 @@ var file = require('./../controller/file');//importando documento file de  contr
 router.get('/produto', function(req, res) {
 	var id = req.query.id;//pegando id do produto enviado por url
 	fs.readFile(__dirname + '/../db/products.json', 'utf8', function(err, data){
-		res.render('produto', {data: data, id:id});//renderizando página e passando parametros data e id
+		data = JSON.parse(data);
+		for(var i=0;i<data.length;i++){
+			if(data[i].id==id){
+				var produto=data[i];
+			}
+		}
+		res.render('produto', {produto: produto, data:data});//renderizando página e passando parametros data e id
 	});
 });
 
