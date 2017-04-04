b1fb193fe504e6b57ed8238ead17ba28845ae2ce
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-04-04 16:52:19 -0300

    leitura de dados json

--- controller/file.js
+++ controller/file.js
@@ -17,25 +17,32 @@ module.exports ={
 			callback(data);
 		});
 	},
-	checkProduto: function(data, params){
-		var check= 0;
-		for(var i=0;i<data.length;i++){
-			if(data[i].nome==params.nome){
-				check = 1;
-			}
-		}
-		if (check==0){
-			return true;
-		}else{
-			return false;
-		}
-	},
-
-	write: function(dataJson, res){
-		fs.writeFile(__dirname + "/../" + 'db/encomendas.json', dataJson, function(err){
-			if(err)
-				return console.log(err);
-			res.json({'msg': 'Usuário inserido com sucesso!'});
+	includeid: function(params, data){
+		var tam=data.length;
+		var id=data[tam-1].id+1;
+		console.log("teste");
+		Object.defineProperty(params, "id", {
+			get: function () { return id; },
+			set: function (value) { id = value; },
+			enumerable: true
 		});
-	}
+		return params;
+	},
+	// checkProduto: function(data, params){
+	// 	data = JSON.parse(data);
+	// 	for(var i=0;i<data.length;i++){
+	// 		if(data[i].id==params){
+	// 			return i;
+
+	// 		}
+	// 	}
+	// },
+
+	// write: function(dataJson, res){
+	// 	fs.writeFile(__dirname + "/../" + 'db/products.json', dataJson, function(err){
+	// 		if(err)
+	// 			return console.log(err);
+	// 		res.json({'msg': 'Usuário inserido com sucesso!'});
+	// 	});
+	// }
 }
