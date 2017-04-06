b0e7b34d4666caf7df7ebaaef9716b4271f0dcb8
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-04-06 10:38:09 -0300

    testes

--- routes/db.js
+++ routes/db.js
@@ -4,10 +4,9 @@ var fs = require('fs');
 var file = require('./../controller/file'); //importando documento file de  controller
 
 
-router.get('/:id', function(req, res) {
+router.get('/', function(req, res) {
 	file.read(function(data){
-		var id = req.params.id
-		console.log(id)
+	// console.log("tagId is set to " + req.param("tagId"));
 		res.json(data);	//printando json de encomendas em pagina do servidor para manipular com javascript
 	});
 });
