ad0536a8022aae66cdd69c8453813d8ab26453e4
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-04-06 10:37:32 -0300

    setando preferencia em item especifico

--- routes/catalogo.js
+++ routes/catalogo.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var file = require('./../controller/file');
 
 /* GET products listing. */
 router.get('/', function(req, res) {
@@ -10,4 +11,22 @@ router.get('/', function(req, res) {
 	});
 });
 
+router.get('/:fav', function(req, res) {
+	fs.readFile(__dirname + '/../db/products.json', 'utf8', function(err, data){
+	 	var data = JSON.parse(data);
+	 	var posArr = file.readProduto(req.params.fav, data.produtos);
+	 	var item = data.produtos[posArr];
+
+	 	if(item.preferido == 'Não'){
+	 		item.preferido = 'Sim';
+	 	}else{
+	 		item.preferido = 'Não';
+	 	}
+
+	 	data.produtos.splice(posArr, 1);
+		data.produtos.push(item);
+		var newDB = JSON.stringify(data);
+		file.write(newDB, res);
+	});
+});
 module.exports = router;
